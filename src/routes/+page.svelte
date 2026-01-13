37b22e9b26041e383dd23bd16ff4b71a03ca2267
William Carver <willtc111@gmail.com>
2026-01-12 23:21:02 -0500

    docs: improve game description on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 
 	let saveSeed: string | undefined = undefined;
 	onMount(() => {
-		saveSeed= localStorage.getItem("saveSeed") ?? undefined;
+		saveSeed = localStorage.getItem("saveSeed") ?? undefined;
 	});
 </script>
 
@@ -26,11 +26,11 @@
 
 <div class="flex h-full w-full flex-col items-center justify-center gap-8 p-2">
 	<section class="flex flex-col gap-2">
-		<h1 class="text-2xl font-bold text-center">
+		<h1 class="text-center text-2xl font-bold">
 			<span class="inline-block">Welcome to</span>
 			<span class="inline-block">Voronoi Sweeper</span>
 		</h1>
-		<div class="flex flex-col md:flex-row justify-center items-center gap-2">
+		<div class="flex flex-col items-center justify-center gap-2 md:flex-row">
 			<button class="btn preset-filled-primary-500" on:click={() => goto(resolve(`/game/${gotd}`))}>
 				Game of the day
 			</button>
@@ -50,19 +50,38 @@
 		</div>
 	</section>
 
-	<section class="flex w-full sm:w-1/2 flex-col items-start justify-start gap-2">
-		<h2 class="text-lg font-bold">What is Voronoi Sweeper?</h2>
+	<section class="flex w-full flex-col items-start justify-start gap-2 sm:w-1/2">
+		<h2 class="text-lg font-bold">How to Play</h2>
 		<span>
-			Voronoi Sweeper is a more generic implementation of the game Minesweeper. Instead of using a
-			full grid of square cells, it uses an arbitrary arrangement of convex cells. This allows for a
-			wider variety of connection patterns between neighboring cells.
+			Voronoi Sweeper is a variation of the classic puzzle game Minesweeper. All of the same
+			gameplay mechanics apply. Reveal a cell to see the number of neighboring mines. If you reveal
+			a mine, you lose. The goal is to reveal all of the non-mine cells as fast as possible. You may
+			flag cells to help keep track of where you think the mines are. Flagged cells can not be
+			clicked for revealing, but may be unflagged at any time.
+		</span>
+	</section>
+	<section class="flex w-full flex-col items-start justify-start sm:w-1/2">
+		<h2 class="mb-2 text-lg font-bold">Controls</h2>
+		<span> Click a cell to reveal it. </span>
+		<span>
+			Right click or click with flagging toggled on (using the "Flag" button) to flag a cell.
 		</span>
 		<span>
-			Click a cell to reveal it, or right click a cell to flag it as a potential mine. Cells are
-			highlighted to indicate which cells neighbor the current cell you hover over. If you see a
-			connection that doesn't make sense, try clicking and dragging to pan around or scrolling to
-			zoom out. The nature of the Voronoi partitioning means that some cells might be touching
-			outside of the default view.
+			Hover over a cell with the mouse cursor (or long press on mobile) to highlight the cell's
+			neighbors.
+		</span>
+		<span> Click and drag to pan the viewport. </span>
+		<span> Scroll or pinch to zoom the viewport. </span>
+	</section>
+	<section class="flex w-full flex-col items-start justify-start gap-2 sm:w-1/2">
+		<h2 class="text-lg font-bold">What is Voronoi Sweeper?</h2>
+		<span>
+			Voronoi Sweeper is a generalized implementation of the Minesweeper concept. Instead of using a
+			full grid of square cells, it uses an arbitrary arrangement of convex cells. This allows for a
+			wider variety of connection patterns between neighboring cells. The nature of the Voronoi
+			partitioning means that some cells might be touching outside of the default view. If you see a
+			connection that doesn't make sense, try panning or zooming out to see the full voronoi
+			partitioning outside of the typical bounds of the board.
 		</span>
 		<span>
 			Boards are generated randomly and mines are set during board generation, so there is no
@@ -73,7 +92,7 @@
 		<span> Enjoy! And let me know if you find any bugs or fun seeds. </span>
 	</section>
 
-	<section class="flex w-full sm:w-1/2 flex-col items-start justify-start gap-2">
+	<section class="flex w-full flex-col items-start justify-start gap-2 sm:w-1/2">
 		<h2 class="text-lg font-bold">Interesting Seeds</h2>
 		<ul class="flex flex-col gap-2">
 			<li>
